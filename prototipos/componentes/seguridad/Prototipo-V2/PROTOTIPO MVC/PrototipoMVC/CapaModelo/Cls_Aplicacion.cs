1f9324d8a670c76cbf98724a523393d617f64c99
cestradae5 <cestradae5@miumg.edu.gt>
2025-09-07 07:44:55 -0600

    respaldo 7 septiembre

--- prototipos/componentes/seguridad/Prototipo-V2/PROTOTIPO MVC/PrototipoMVC/CapaModelo/Cls_Aplicacion.cs
+++ prototipos/componentes/seguridad/Prototipo-V2/PROTOTIPO MVC/PrototipoMVC/CapaModelo/Cls_Aplicacion.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 namespace CapaModelo
 {
+    //clase aplicacion
     public class Cls_Aplicacion
     {
         public int PkIdAplicacion { get; set; }
